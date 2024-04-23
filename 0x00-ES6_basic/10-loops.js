202cc9342046a2c66e45b3c373dc0d1aacc1c47e
mouzerzo <mouzerzo@e2r9p11.1337.ma>
2024-04-23 03:55:19 +0100

    new things

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+  for (const idx of array) {
+    array[idx] = appendString + array[idx];
   }
 
   return array;
