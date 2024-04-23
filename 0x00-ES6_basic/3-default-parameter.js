97861a97ebaadb034438b01a6ecfde96a87f4ebd
mouzerzo <mouzerzo@e2r9p11.1337.ma>
2024-04-23 01:54:46 +0100

    new

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,1 +1,2 @@
-export default function getSumOfHoods(_ber, _89 = 89, _19 = 19) { return (_ber + _89 + _19); };
+// eslint-disable-next-line
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) { return (initialNumber + expansion1989 + expansion2019); };
