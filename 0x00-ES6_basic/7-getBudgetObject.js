25f90d3352573bab38d8a630f0672fd9be41b0af
mohamed <mohamed.ouz2016@gmail.com>
2024-04-23 02:13:44 +0000

    fix error

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,12 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-        income: income,
-        gdp: gdp,
-        capita: capita,
-    };
-    budget.income = income;
-    budget.gdp = gdp;
-    budget.capita = capita;
-
-    return budget;
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+  return budget;
 }
