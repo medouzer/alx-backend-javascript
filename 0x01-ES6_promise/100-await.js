8b237a7074ff83df86283097fffddff36a4aaf3b
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-25 00:29:09 +0100

    new task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,8 +3,8 @@ import { createUser, uploadPhoto } from './utils';
 const asyncUploadUser = async () => {
   try {
     return {
-      photo: await uploadPhoto,
-      user: await createUser,
+      photo:  uploadPhoto,
+      user:  createUser,
     }
   } catch (e) {
     return {
