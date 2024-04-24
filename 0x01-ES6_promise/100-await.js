69a874784cc1293857b1cca021c6b025e021fa7f
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-25 00:25:51 +0100

    new task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { createUser, uploadPhoto } from './utils';
+
+const asyncUploadUser = async () => {
+  try {
+    return {
+      photo: await uploadPhoto,
+      user: await createUser,
+    }
+  } catch (e) {
+    return {
+      photo: null,
+      user: null
+    }
+  }
+}
+
+export default asyncUploadUser
