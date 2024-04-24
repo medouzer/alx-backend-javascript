71d871d3c0865a01500b3b49ff3545b6ed956960
mohamed <mohamed.ouz2016@gmail.com>
2024-04-24 19:35:59 +0000

    good

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,9 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
-  .all([uploadPhoto(), createUser()])
-  .then((user) => {
-    console.log(`${user[0].body} ${user[1].firstName} ${user[1].lastName}`);
-  })
-  .catch(() => console.log('Signup system offline'))
+    .all([uploadPhoto(), createUser()])
+    .then((user) => {
+      console.log(`${user[0].body} ${user[1].firstName} ${user[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
