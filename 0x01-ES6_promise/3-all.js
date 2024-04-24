1683747728107f6dc1c417a255340d0e5515ff5f
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-24 19:28:39 +0100

    to test

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import createUser from './utils'
 
 export default function handleProfileSignup() {
   return promise
-  .all([uploadPhoto, createUser])
-  .then((photo, user) => console.log(`body: ${photo} firstName: ${user.firstName} lastName: ${user.lastName}`))
+  .all([uploadPhoto(), createUser()])
+  .then((photo, user) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`))
   .catch(() => console.log('Signup system offline'))
 }
