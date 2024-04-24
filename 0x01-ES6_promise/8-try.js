8bfe8d530c16521db6a4c57f62d78a4a4c2ca6b7
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-25 00:02:01 +0100

    new task

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,5 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    console.log(`numerator : ${numerator} | denominator ${denominator}`);
     if (denominator != 0)
       return (numerator / denominator);
     throw Error("cannot divide by 0");
-  } catch (e) {
-    console.log(e);
-  }
 }
