5523b4fdca1a135c5fe6702720ed102a4a58a468
mohamed <mohamed.ouz2016@gmail.com>
2024-04-24 17:06:32 +0000

    fix erros

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,10 @@
-export default function getFullResponseFromAPI(success){
-    return new Promise((resolve, reject) => {
-        if (success)
-        {
-            const data = {status: 200, body: 'Success'}
-            resolve(data);
-        }
-        else
-        {
-            reject(new Error("The fake API is not working currently"));
-        }
-    });
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const data = { status: 200, body: 'Success' };
+      resolve(data);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
