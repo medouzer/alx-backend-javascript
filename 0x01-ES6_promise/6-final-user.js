3cc44ebf458c7d2284e20b9a78579bc656dcfca5
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-24 23:04:20 +0100

    new task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,17 +3,21 @@ import uploadPhoto from './5-photo-reject'
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-  .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then((data) => {
-    return {
-      status: "pending",
-      value: data
-    }
-  })
-  .catch((e) => {
-    return {
-      status: "rejected",
-      value: e.message
-    }
-  })
+    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => {
+      const arr = [];
+      for (let index = 0; i < data.length; index++) {
+        arr.push({
+          status: "pending",
+          value: data[index]
+        })
+      }
+      return arr;
+    })
+    .catch((e) => {
+      return {
+        status: "rejected",
+        value: e.message
+      }
+    })
 }
