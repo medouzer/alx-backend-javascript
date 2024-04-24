ebde3554d0fa670c4a1accd44cc03347ef1ac698
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-25 00:13:56 +0100

    new task

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     queue.push(mathFunction());
     queue.push('Guardrail was processed');
   } catch (e) {
-    queue.push(e.message);
+    queue.push(e);
     queue.push('Guardrail was processed');
   }
   return queue;
