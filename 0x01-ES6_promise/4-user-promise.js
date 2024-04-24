510f5a61510b5593f04898c8dc5dd37df7179582
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-24 22:24:22 +0100

    new task

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,10 @@
 export default function signUpUser(firstName, lastName) {
-    return  new Promise(function(resolve, reject) {
+    const promise = new Promise(function(resolve, reject) {
         return {
             firstName,
             lastName
         }
     });
+
+    return (promise.resolve())
 }
