527ae459383ed9e0c84a4c5b1643c5daae90a00a
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-24 19:18:22 +0100

    to test

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup() {
   return promise
   .all([uploadPhoto, createUser])
   .then((values) => console.log(values))
-//   .catch((error) => new Error())
+  .catch((error) => new Error())
 //   .finally(() => console.log('Got a response from the API'))
 }
