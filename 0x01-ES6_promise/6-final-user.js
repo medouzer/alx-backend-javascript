a1521a0ac4eb314910e627be5c433fdb7c6d71b9
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-24 23:22:26 +0100

    new task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,9 +5,15 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => {
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }));
+      const data = [];
+      res.map((o) => {
+        data.push({
+          status: o.status,
+          value: o.status === 'fulfilled' ? o.value : String(o.reason),
+        })
+      });
+
+      console.log(data);
+      return (data);
     });
 }
