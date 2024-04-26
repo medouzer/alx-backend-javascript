4c2c3ecb2efab7d3b2bc1118f3d9631de6dfdfe9
mohamed <mohamed.ouz2016@gmail.com>
2024-04-26 01:36:09 +0000

    fix lint

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,20 +1,20 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-	constructor(amount, currency) {
+  constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
-	set amount(amount) {
-		if ((typeof amount !== 'number') && !(amount instanceof Number)) {
+  set amount(amount) {
+    if ((typeof amount !== 'number') && !(amount instanceof Number)) {
       throw new TypeError('Amount must be a number');
     }
     this._amount = amount;
   }
 
   set currency(currency) {
-		if (!(currency instanceof Currency)) {
+    if (!(currency instanceof Currency)) {
       throw new TypeError('Currency must be an instanceof of Currency');
     }
     this._currency = currency;
@@ -23,16 +23,16 @@ export default class Pricing {
   get amount() {
     return this._amount;
   }
-  
+
   get currency() {
     return this._currency;
   }
 
-	displayFullPrice() {
-		return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-	}
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-	static convertPrice(amount, conversionRate) {
-		return (amount * conversionRate)
-	}
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
 }
