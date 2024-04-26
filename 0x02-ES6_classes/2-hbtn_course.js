f2dc96ec3d957ae236d3d90a739e0a990e7f1d91
mouzerzo <mouzerzo@e1r5p12.1337.ma>
2024-04-26 01:51:27 +0100

    fix error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -14,36 +14,36 @@ export default class HolbertonCourse {
       this.students = students;
   }
 
-  setName(name) {
+  set name(name) {
       if (typeof name != "string") {
           throw new TypeError('Name must be a String');
       }
       this._name = name;
   }
 
-  setlength(length) {
+  set length(length) {
       if (typeof length != "number") {
           throw new TypeError('length must be a Number');
       }
       this._length = length;
   }
 
-  setStudents(students) {
+  set students(students) {
       if (typeof students != "object" && typeof students[0] == "string") {
           throw new TypeError('students must be a array of Strings');
       }
       this._students = students;
   }
 
-  getName() {
+  get name() {
     return this._name;
   }
 
-  getlength() {
+  get length() {
     return this._length;
   }
 
-  getStudents() {
+  get students() {
     return this._students;
   }
 }
