80d4dcc439795fd47a577d0158b762b9789b6c71
mohamed <mohamed.ouz2016@gmail.com>
2024-04-30 21:26:01 +0000

    new task

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,8 @@
 export default function hasValuesFromArray(set, array) {
-  for (const value in array) {
-    if (!set.has(value))
+  for (const value of array) {
+    if (!set.has(value)) {
       return false;
+    }
   }
   return true;
 }
