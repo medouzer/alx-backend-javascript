98b94b014a1ccb75f67079ef72e38dc08ed5c1ab
mohamed <mohamed.ouz2016@gmail.com>
2024-04-30 20:20:29 +0000

    final check

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,19 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    const studentsByCity = students.filter((student) => student.location === city);
+  const studentsByCity = students.filter((student) => student.location === city);
 
-    const updatedStudents = studentsByCity.map((student) => {
-        const matchingGrade = newGrades.filter((newGrade) => student.id === newGrade.studentId);
-        if (matchingGrade.length > 0) {
-            return {
-              ...student,
-              grade: matchingGrade[0].grade,
-            };
-          }
-          return {
-            ...student,
-            grade: 'N/A',
-          };
-    });
+  const updatedStudents = studentsByCity.map((student) => {
+    const matchingGrade = newGrades.filter((newGrade) => student.id === newGrade.studentId);
+    if (matchingGrade.length > 0) {
+      return {
+        ...student,
+        grade: matchingGrade[0].grade,
+      };
+    }
+    return {
+      ...student,
+      grade: 'N/A',
+    };
+  });
 
-    return updatedStudents;
+  return updatedStudents;
 }
