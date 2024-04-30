bb7eb5f4f6015d7cf7eb24849b3bf735fa7ceca8
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-04-30 20:23:28 +0100

    check

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,10 +4,11 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     const new_student = studentby_city.map((student) => {
         const grades = newGrades.filter((newgrade) => student.id === newgrade.id)
         if (grades.lenght > 0) {
-            student.grade = newgrade.grade;
+            student.grade = grades[0].grade;
         } else {
             student.grade = 'N/A';
         }
+        return student;
     });
     return new_student;
 }
