795f669f539e0be1c9cf95798d09f238854c6268
ouzerzou <mohamed.ouz2016@gmail.com>
2024-06-19 03:46:20 +0100

    new check for task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,61 +1,39 @@
 const express = require('express');
-const fs = require('fs');
-
-const countStudents = (dataPath) => new Promise((resolve, reject) => {
-  if (!dataPath) {
-    reject(new Error('Cannot load the database'));
-  }
-  if (dataPath) {
-    fs.readFile(dataPath, (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      }
-      if (data) {
-        const reportParts = [];
-        const fileLines = data.toString('utf-8').trim().split('\n');
-        const studentGroups = {};
-        const dbFieldNames = fileLines[0].split(',');
-        const studentPropNames = dbFieldNames.slice(
-          0,
-          dbFieldNames.length - 1,
-        );
-
-        for (const line of fileLines.slice(1)) {
-          const studentRecord = line.split(',');
-          const studentPropValues = studentRecord.slice(
-            0,
-            studentRecord.length - 1,
-          );
-          const field = studentRecord[studentRecord.length - 1];
-          if (!Object.keys(studentGroups).includes(field)) {
-            studentGroups[field] = [];
-          }
-          const studentEntries = studentPropNames.map((propName, idx) => [
-            propName,
-            studentPropValues[idx],
-          ]);
-          studentGroups[field].push(Object.fromEntries(studentEntries));
-        }
-
-        const totalStudents = Object.values(studentGroups).reduce(
-          (pre, cur) => (pre || []).length + cur.length,
-        );
-        reportParts.push(`Number of students: ${totalStudents}`);
-        for (const [field, group] of Object.entries(studentGroups)) {
-          reportParts.push([
-            `Number of students in ${field}: ${group.length}.`,
-            'List:',
-            group.map((student) => student.firstname).join(', '),
-          ].join(' '));
-        }
-        resolve(reportParts.join('\n'));
-      }
-    });
-  }
-});
+const fs = require('fs').promises;
 
 const app = express();
 const databaseFile = process.argv[2];
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+
+    const hashtable = {};
+    let studentCount = 0;
+
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!hashtable[field]) {
+        hashtable[field] = [];
+      }
+      hashtable[field].push(firstname);
+      studentCount += 1;
+    });
+
+    let result = `Number of students: ${studentCount}\n`;
+    for (const [key, students] of Object.entries(hashtable)) {
+      result += `Number of students in ${key}: ${students.length}. List: ${students.join(', ')}\n`;
+    }
+    return result.trim();
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -63,10 +41,10 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res) => {
   try {
-    const result = await countStudents(databaseFile);
-    res.send(`This is the list of our students\n${result}`);
+    const data = await countStudents(databaseFile);
+    res.send(`This is the list of our students\n${data}`);
   } catch (error) {
-    res.status(500).send('Cannot load the database');
+    res.status(500).send(`This is the list of our students\n${error.message}`);
   }
 });
 
